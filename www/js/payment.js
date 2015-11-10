26de1f15c16196e7199b95e861bc6a1f8262b61f
Jose <jose@practisis.com>
2015-11-10 16:24:10 -0500

    nombre nuevo otra vez

--- www/js/payment.js
+++ www/js/payment.js
@@ -666,7 +666,7 @@ function impresionMovil(){
 	$('#totalPagado').html('');
 	$('#tablaCompra').html('');
 	$('#printFactura').hide();
-	 window.open('centvia://?udn=Impresion&utt=NubePOS&cru=NubePOSV2&cruf=NubePOSV2&c_='+respuesta,'_system','location=yes');
+	 window.open('centvia://?udn=Impresion&utt=NubePOS&cru=nubeposv2&cruf=nubeposv2&c_='+respuesta,'_system','location=yes');
 	 envia('puntodeventa');
 }
 
